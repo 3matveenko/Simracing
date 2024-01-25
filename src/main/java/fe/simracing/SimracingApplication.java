b2768b6fe0f4be9c8a4d87626939fbf2120fc2c7
3matveenko <3matveenkokz@gmail.com>
2024-01-25 16:49:21 +0600

    first commit

---
+++ src/main/java/fe/simracing/SimracingApplication.java
@@ -0,0 +1,13 @@
+package fe.simracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fe/simracing/SimracingApplicationTests.java
@@ -0,0 +1,13 @@
+package fe.simracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
